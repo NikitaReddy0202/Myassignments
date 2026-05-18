6d73bb0ebeaf53f2ec6749df907c14ab236aac72
NikitaReddy0202 <nikitareddy97@gmail.com>
2026-05-18 20:19:03 +0530

    loop assignment

---
+++ loop.js
@@ -0,0 +1,6 @@
+let num=30;
+for(i=0;i<=num;i++){
+    if(i%2!==0){
+        console.log("the number is odd is",i)
+    }
+}
---
+++ loopEvenOrOdd.js
@@ -0,0 +1,9 @@
+let num=25;
+for(i=0;i<=num;i++){
+    if(i%2==0){
+        console.log("the number is even is",i)
+    }
+    else{
+        console.log("the number is odd",i)
+    }
+}
